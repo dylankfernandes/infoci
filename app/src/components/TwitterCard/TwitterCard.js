d1d43ee576495993097d4350cdb4f7fd091e103b
Developer5262 <dylan.fernandes00@gmail.com>
2017-11-13 20:27:50 -0500

    attempted twitter timeline

--- app/src/components/TwitterCard/TwitterCard.js
+++ app/src/components/TwitterCard/TwitterCard.js
@@ -4,7 +4,47 @@ import { Card } from 'antd';
 import { Timeline } from 'react-twitter-widgets';
 import './style.css';
 
+const R = require("request");
+const CONSUMER_KEY = 'gfSWnp25JKgvHamqPsWVNoI43';
+const CONSUMER_SECRET = 'cjuQ31Fe0ZA6cMLQB2Zgz9wv5thXs5HNKEJJb4oD7c04B8fZPR';
+
 class TwitterCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {}
+    }
+  }
+
+  getBearerToken = () => {
+    const concat = `${CONSUMER_KEY}:${CONSUMER_SECRET}`;
+    const CREDENTIALS = new Buffer(concat).toString('base64');
+    const credits = btoa(concat)
+
+    const url = 'https://api.twitter.com/oauth2/token';
+    
+    R({url: url,
+      method:'POST',
+      headers: {
+        "Authorization": "Basic " + credits,
+        "Content-Type":"application/x-www-form-urlencoded;charset=UTF-8",
+        "Content-Length": "29",
+        "Accept-Encoding": "gzip",
+        "User-Agent": "Infoci"
+      },
+      body: "grant_type=client_credentials"
+    }, function(err, resp, body) {
+      console.log(err)
+      console.log(body); //the bearer token...
+      return body;
+    });
+  }
+
+  componentWillMount() {
+    let username = this.props.username;
+    const token = this.getBearerToken();
+  }
+
   render() {
     return (
       <div>
@@ -19,7 +59,6 @@ class TwitterCard extends Component {
             width: '50vw'
           }}
           onLoad={() => console.log('Timeline is loaded!')} />
-        
       </div>
     )
   }
